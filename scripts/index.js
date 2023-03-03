8b8fc934c2e59178d803128ade0b1ec360b8916e
MathewCibi <79239937+MathewCibi@users.noreply.github.com>
2023-03-03 14:58:40 +1300

    update

--- scripts/index.js
+++ scripts/index.js
@@ -1,37 +1,46 @@
+
 const rand = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
-const enhance = id => {
-  const element = document.getElementById(id),
-        text = element.innerText.split("");
-  
-  element.innerText = "";
-  
-  text.forEach((value, index) => {
-    const outer = document.createElement("span");
+window.addEventListener('load', function () {
+  const enhance = id => {
+    console.log(document.getElementById("channel_link"))
+    const element = document.getElementById(id),
+          text = element.innerText.split("");
+          console.log(text)
     
-    outer.className = "outer";
+    element.innerText = "";
     
-    const inner = document.createElement("span");
+    text.forEach((value, index) => {
+      const outer = document.createElement("span");
+      
+      outer.className = "outer";
+      
+      const inner = document.createElement("span");
+      
+      inner.className = "inner";
+      
+      inner.style.animationDelay = `${rand(-5000, 0)}ms`;
+      
+      const letter = document.createElement("span");
+      
+      letter.className = "letter";
+      
+      letter.innerText = value;
+      
+      letter.style.animationDelay = `${index * 1000 }ms`;
+      
+      inner.appendChild(letter);    
+      
+      outer.appendChild(inner);    
+      
+      element.appendChild(outer);
+    });
     
-    inner.className = "inner";
     
-    inner.style.animationDelay = `${rand(-5000, 0)}ms`;
-    
-    const letter = document.createElement("span");
-    
-    letter.className = "letter";
-    
-    letter.innerText = value;
-    
-    letter.style.animationDelay = `${index * 1000 }ms`;
-    
-    inner.appendChild(letter);    
-    
-    outer.appendChild(inner);    
-    
-    element.appendChild(outer);
-  });
-}
+  }
 
-enhance("channel_link");
-print('Working')
+  enhance("channel_link");
+})
+
+
+
